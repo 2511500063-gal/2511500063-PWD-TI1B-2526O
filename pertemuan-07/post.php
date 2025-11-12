d28d1dcbe1caac54d2cd985c12c33f016fee4049
GALANG NURHAFIZD <2511500063@atmaluhur.ac.id>
2025-11-12 21:05:35 +0700

    isset

--- pertemuan-07/post.php
+++ pertemuan-07/post.php
@@ -1,8 +1,18 @@
 <?php
   session_start();
-  $sesname = $_SESSION["nama"];
-  $sesemail = $_SESSION["email"];
-  $sespesan = $_SESSION["pesan"];
+  $sesname = "";
+  if (isset($_SESSION["nama"]));
+    $sesname = $_SESSION["nama"];
+  endif;
+
+  $sesemail = "";
+  if (isset($_SESSION["email"]));
+     $sesemail = $_SESSION["email"];
+  endif;
+  $sespesan = "";
+  if (isset($_SESSION["pesan"]))
+    $sespesan = $_SESSION["pesan"];
+  endif;
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -88,11 +98,13 @@
         <button type="submit">Kirim</button>
         <button type="reset">Batal</button>
       </form>
+      <?php if (!empty($sesname)); ?>
       <p>Terimakasih sudah menghubungi kami:
         <label>Nama: <strong>?=<?php echo $sesname; ?></strong></label>
         <label>Email: <strong>?=<?php echo $sesemail; ?></strong></label>
         <label>Pesan: <strong>?=<?php echo $sespesan; ?></strong></label>
       </p>
+      <?php endif; ?>
     </section>
   </main>
 
