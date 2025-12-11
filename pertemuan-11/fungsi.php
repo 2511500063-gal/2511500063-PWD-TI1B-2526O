a52013b882ca321a5704d98ee64ec3c6137dde16
GALANG NURHAFIZD <2511500063@atmaluhur.ac.id>
2025-12-11 15:37:48 +0700

    update fungsi.php

--- pertemuan-11/fungsi.php
+++ pertemuan-11/fungsi.php
@@ -6,25 +6,40 @@ function redirect_ke($url)
 }
 
 
+function bersihkan($input) 
+{
+    
+    if (!is_string($input)) {
+        return $input; 
+    }
+    
+    
+    $input = trim($input);
+    $input = stripslashes($input);
+    $input = htmlspecialchars($input, ENT_QUOTES, 'UTF-8'); 
+    return $input;
+}
+
+
 function tidakKosong($str)
 {
-  return strlen(trim($str)) > 0;
+    return strlen(trim($str)) > 0;
 }
 
 function formatTanggal($tgl)
 {
-  return date("d M Y", strtotime($tgl));
+    return date("d M Y", strtotime($tgl));
 }
 
 function tampilkanBiodata($conf, $arr)
 {
-  $html = "";
-  foreach ($conf as $k => $v) {
-    $label = $v["label"];
-    $nilai = bersihkan($arr[$k] ?? '');
-    $suffix = $v["suffix"];
+    $html = "";
+    foreach ($conf as $k => $v) {
+        $label = $v["label"];
+        $nilai = bersihkan($arr[$k] ?? ''); 
+        $suffix = $v["suffix"];
 
-    $html .= "<p><strong>{$label}</strong> {$nilai}{$suffix}</p>";
-  }
-  return $html;
+        $html .= "<p><strong>{$label}</strong> {$nilai}{$suffix}</p>";
+    }
+    return $html;
 }
