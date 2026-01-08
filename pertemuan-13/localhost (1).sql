2a5d38a16410eefb4b75548392c92b78b379cbda
GALANG NURHAFIZD <2511500063@mahasiswa.atmaluhur.ac.id>
2026-01-08 11:55:13 +0700

    update localhost

--- pertemuan-13/localhost (1).sql
+++ pertemuan-13/localhost (1).sql
@@ -20,7 +20,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `db_pwd2025`
 --
-CREATE DATABASE IF NOT EXISTS `db_pwd2025` DEFAULT CHARACTER SET utf8mb4 ;
+CREATE DATABASE IF NOT EXISTS `db_pwd2025` DEFAULT CHARACTER SET utf8mb4;
 USE `db_pwd2025`;
 
 -- --------------------------------------------------------
