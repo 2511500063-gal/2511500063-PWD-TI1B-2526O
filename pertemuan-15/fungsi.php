37eae4773c00f5038b11dbea97adf2f082f6e4a1
Galang Nurhafizd <2511500063@mahasiswa.atmaluhur.ac.id>
2026-01-12 10:12:31 +0700

    update fungsi.php

--- pertemuan-15/fungsi.php
+++ pertemuan-15/fungsi.php
@@ -24,8 +24,8 @@ function tampilkanBiodata($conf, $arr)
 {
   $html = "";
   foreach ($conf as $k => $v) {
-    $label = $v["label"];
-    $nilai = bersihkan($arr[$k] ?? '');
+    $label  = $v["label"];
+    $nilai  = bersihkan($arr[$k] ?? '');
     $suffix = $v["suffix"];
 
     $html .= "<p><strong>{$label}</strong> {$nilai}{$suffix}</p>";
